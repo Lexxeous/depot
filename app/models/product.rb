934c595e0480e9a1f7654b3990f2b75f832ceb57
Jonathan Alexander Gibson <jagibson44@students.tntech.edu>
2018-05-23 16:51:36 -0400

    Chapter 8 Application Styling and Testing Done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+	def self.latest
+		order(:updated_at).last
+	end
+
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Product.delete_all
 Product.create(title: 'Seven Mobile Apps in Seven Weeks',
   description:
     "<p>
-      <em>Native Apps, Multiple Platforms</em>
+      <strong><em>Native Apps, Multiple Platforms:</em></strong>
       Answer the question “Can we build this for ALL the devices?” with a
       resounding YES. This book will help you get there with a real-world
       introduction to seven platforms, whether you’re new to mobile or an
@@ -19,30 +19,49 @@ Product.create(title: 'Seven Mobile Apps in Seven Weeks',
       out which cross-platform solution makes the most sense for your needs.
       </p>",
   image_url: '7apps.jpg',
-price: 26.00)
+price: 25)
 
-Product.create(title: 'Seven Mobile Apps in Seven Weeks',
+Product.create(title: 'Ruby Performance Optimization',
   description:
     "<p>
-      <em>Native Apps, Multiple Platforms</em>
-      Answer the question “Can we build this for ALL the devices?” with a
-      resounding YES. This book will help you get there with a real-world
-      introduction to seven platforms, whether you’re new to mobile or an
-      experienced developer needing to expand your options. Plus, you’ll find
-      out which cross-platform solution makes the most sense for your needs.
+      <strong><em>Why Your Ruby is Slow and How to Fit It:</em></strong>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae rhoncus ipsum. 
+      Integer mollis diam dolor, et condimentum erat posuere eu. Suspendisse vel finibus lectus. 
+      Etiam ornare sodales malesuada. Praesent molestie a ex ut placerat. Nulla quis dictum arcu. 
+      Donec sodales in purus nec vestibulum. Praesent posuere ipsum vitae leo congue fringilla. 
+      Cras vehicula risus lectus, et tempus dui lobortis at. Phasellus facilisis non nibh id aliquam. 
+      Fusce ut purus id nisl porttitor volutpat in feugiat erat. Vestibulum pretium, lectus vitae tempus suscipit, 
+      turpis mauris pharetra metus, ut placerat elit ante id quam.
       </p>",
   image_url: 'adrpo.jpg',
-price: 26.00)
+price: 30)
 
-Product.create(title: 'Seven Mobile Apps in Seven Weeks',
+Product.create(title: 'Rails Angular Postgres Bootstrap',
   description:
     "<p>
-      <em>Native Apps, Multiple Platforms</em>
-      Answer the question “Can we build this for ALL the devices?” with a
-      resounding YES. This book will help you get there with a real-world
-      introduction to seven platforms, whether you’re new to mobile or an
-      experienced developer needing to expand your options. Plus, you’ll find
-      out which cross-platform solution makes the most sense for your needs.
+      <strong><em>Interfacing RoR with Bootstrap:</em></strong>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae rhoncus ipsum. 
+      Integer mollis diam dolor, et condimentum erat posuere eu. Suspendisse vel finibus lectus. 
+      Etiam ornare sodales malesuada. Praesent molestie a ex ut placerat. Nulla quis dictum arcu. 
+      Donec sodales in purus nec vestibulum. Praesent posuere ipsum vitae leo congue fringilla. 
+      Cras vehicula risus lectus, et tempus dui lobortis at. Phasellus facilisis non nibh id aliquam. 
+      Fusce ut purus id nisl porttitor volutpat in feugiat erat. Vestibulum pretium, lectus vitae tempus suscipit, 
+      turpis mauris pharetra metus, ut placerat elit ante id quam.
       </p>",
   image_url: 'dcbang.jpg',
-price: 26.00)
+price: 35)
+
+Product.create(title: 'Agile Web Development with Rails 5',
+  description:
+    "<p>
+      <strong><em>Learn RoR5 from the Ground Up:</em></strong>
+      Have you ever wanted an in depth look at how a high level language like Ruby and a web
+      front end interface like Rails can work together to create beautiful websites while also 
+      using tools like HTML, CSS, and YAML? Well...look no further. <em>Agile Web Development with Rails 5</em>
+      offers step by step instructions for installing Ruby, Rails, Gems, and all thier dependencies as well as
+      walking through a complex project that teaches almost everything you could ever learn about using
+      Ruby on Rails (RoR). So what are you waiting for? Learn to be a web developer, it's easier
+      that you think!
+      </p>",
+  image_url: 'rails5.jpg',
+price: 40)
