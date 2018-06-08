bf529592cf224de1bbd90a37bf9010767206b170
Jonathan Alexander Gibson <jagibson44@students.tntech.edu>
2018-06-08 09:48:09 -0400

    Chapter 13 Sending Mail Done

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do 
+	config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  	address: 							"agibson@gibsonusa.net",
+  	port: 		 						587,
+  	domain:	 							"depot@example.net",
+  	authenication:  			"plain",
+  	user_name: 						"Alex",
+  	password: 						"password",
+  	enable_starttls_auto: true
+  }
+end
