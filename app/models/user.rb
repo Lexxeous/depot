7e4e00b024280113d234d1b93537804a06bd0421
Jonathan Alexander Gibson <jagibson44@students.tntech.edu>
2018-06-11 16:27:55 -0400

    Chapter 14 Login Done with link bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
 	has_secure_password
 	validates :name, presence: true, uniqueness: true
+	after_destroy :ensure_an_admin_remains
+end
+
+class Error < StandardError
+
+	private
+		def ensure_an_admin_remains
+			if User.count.zero?
+				raise Error.new "Can't delete last administrative user."
+			end
+		end
 end
